f65cf94a836b01556a145a1e1f3392177614083c
Ivan Dsouza <113847518+Ivan-3101@users.noreply.github.com>
2023-07-28 03:39:15 +0530

    1:08:23

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from .models import Flight,Airport
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Praise the Lord")
+    # return HttpResponse("Praise the Lord")
+    return render(request, "flights/index.html",{
+        "flights" : Flight.objects.all()
+    })
